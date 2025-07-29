dd0ed99ae7609359b0a02be3b713ebbf99430fd2
PavelRozhkov <prozskov@gmail.com>
2025-07-29 15:04:48 +0200

    Remove unused comments in search_definition_metadata_extractor.rb

--- lib/inferno_suite_generator/extractors/search_definition_metadata_extractor.rb
+++ lib/inferno_suite_generator/extractors/search_definition_metadata_extractor.rb
@@ -64,21 +64,6 @@ module InfernoSuiteGenerator
               end
             end
 
-            # path = param.expression.gsub(/.where\(resolve\((.*)/, '').gsub(/url = '/, 'url=\'')
-            # path = path[1..-2] if path.start_with?('(') && path.end_with?(')')
-            # path.scan(/[. ]as[( ]([^)]*)[)]?/).flatten.map do |as_type|
-            #   path.gsub!(/[. ]as[( ](#{as_type}[^)]*)[)]?/, as_type.upcase_first) if as_type.present?
-            # end
-
-            # full_paths = path.split('|')
-            # # There is a problem with whitespaces in paths
-            # full_paths = full_paths.map(&:strip)
-
-            # There is a bug in AU Core 5 asserted-date search parameter. See FHIR-40573
-            if param.respond_to?(:version) && param.version == "5.0.1" && name == "asserted-date"
-              remove_additional_extension_from_asserted_date(full_paths)
-            end
-
             full_paths
           end
       end
@@ -165,7 +150,6 @@ module InfernoSuiteGenerator
       def contains_multiple?
         if profile_element.present?
           if profile_element.id.start_with?("Extension") && extension_definition.present?
-            # Find the extension instance in a AU Core profile
             target_element = profile_elements.find do |element|
               element.type.any? { |type| type.code == "Extension" && type.profile.include?(extension_definition.url) }
             end
