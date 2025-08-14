5eceff1089091f178ef79a1360ff0dff0db85e14
Pavel Rozhkov <prozskov@gmail.com>
2025-08-14 17:51:53 +0200

    Simplify `create_test` logic by replacing manual JSON parsing with `FHIR.from_contents`.

--- lib/inferno_suite_generator/test_modules/create_test.rb
+++ lib/inferno_suite_generator/test_modules/create_test.rb
@@ -9,14 +9,8 @@ module InfernoSuiteGenerator
       puts "data_str: #{data_str}"
       skip "No #{resource_type} resource provided for create test" if data_str.nil? || data_str.to_s.strip.empty?
 
-      begin
-        data_hash = JSON.parse(data_str)
-      rescue JSON::ParserError => e
-        assert false, "Input #{resource_type} JSON is invalid: #{e.message}"
-      end
-
-      resource_instance = FHIR.const_get(resource_type).new(data_hash)
-      fhir_create(resource_type, resource: resource_instance)
+      resource_instance = FHIR.from_contents(data_str)
+      fhir_create(resource_instance)
 
       assert_response_status(201)
       assert_resource_type(resource_type)
