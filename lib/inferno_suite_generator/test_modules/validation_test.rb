dcd16157a3808b99aab222d4db4266474b9cc8c5
Pavel Rozhkov <prozskov@gmail.com>
2025-08-19 17:12:13 +0200

    Refactor: Replace `skip_if_empty` with `validation_behavior` for enhanced flexibility in resource validation logic

--- lib/inferno_suite_generator/test_modules/validation_test.rb
+++ lib/inferno_suite_generator/test_modules/validation_test.rb
@@ -13,18 +13,19 @@ module InfernoSuiteGenerator
     def perform_validation_test(resources,
                                 profile_url,
                                 profile_version,
-                                skip_if_empty: true)
-      validate_resource_conditions(resources, profile_url, skip_if_empty)
+                                validation_behavior: :skip_if_empty)
+      validate_resource_conditions(resources, profile_url, validation_behavior == :skip_if_empty)
       process_resources(resources, profile_url, profile_version)
     end
 
     private
 
     def validate_resource_conditions(resources, profile_url, skip_if_empty)
-      conditional_skip_with_msg skip_if_empty && resources.blank?,
+      resources_blank = resources.blank?
+      conditional_skip_with_msg skip_if_empty && resources_blank,
                                 "No #{resource_type} resources conforming to the #{profile_url} profile were returned"
 
-      omit_if resources.blank?,
+      omit_if resources_blank,
               "No #{resource_type} resources provided so the #{profile_url} profile does not apply"
     end
 
